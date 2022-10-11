1081a9cf803b024021ef7d2ed06d9e8cbc463908
Tommy Keswick <t4k@users.noreply.github.com>
2022-10-10 22:27:46 -0700

    try arrow

--- .github/workflows/work.py
+++ .github/workflows/work.py
@@ -1,8 +1,13 @@
-import datetime
-
+import arrow
 import bleach
 import feedparser
 
 from bs4 import BeautifulSoup
 
-print(f"🐞 {datetime.datetime.now(datetime.timezone.utc)}")
+print(f"🐞 arrow.utcnow(): {arrow.utcnow()}")
+print(f"🐞 arrow.now(): {arrow.now()}")
+
+notices = feedparser.parse("https://libcal.caltech.edu/rss.php?cid=5754&m=day")
+for entry in notices.entries:
+    datetimetz_string = f'{entry["libcal_date"]} {entry["libcal_end"]} America/Los_Angeles'
+    print(f'🐞 arrow.get(): {arrow.get(datetimetz_string, "YYYY-MM-DD HH:mm:ss ZZZ")}')
